27bfa736d79880f8594f9b7ae7b1a2b085e6d7bc
Lenka Galcikova <lenka.galcikova@stuba.sk>
2023-01-11 12:25:59 +0100

    cleanup2

--- MPC_fcn_first.m
+++
@@ -1,6 +0,0 @@
-% function for Simulink implementation
-function u_opt = MPC_fcn_first(x0,xref) % optimal control action
-    global mpc N
-    xref = [xref;N*xref];
-    u_opt = mpc.evaluate(x0,'x.reference',xref);
-end
