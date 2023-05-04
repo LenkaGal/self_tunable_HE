c1ad259e60fb7ed6bd9affc861ac4ca4ca397515
Lenka Galcikova <lenka.galcikova@stuba.sk>
2023-05-04 09:58:42 +0200

    data: ploty: edited to be more user friendly

--- data/ploty.m
+++ data/ploty.m
@@ -16,9 +16,10 @@ xlim([0 3000])
 
 %% Paper
 close all, clear
-load('ref_tracking30.mat') %1 -> Q_y = 1000
+tuned = 1; %default - for boundary controllers
+load('ref_tracking31.mat') %1 -> Q_y = 1000
 from = 801;
-to = 5800;
+to = 6000;
 time = out.ScopeData(1:to-from+1,1);
 ref = out.ScopeData(from:to,4);
 input1 = out.ScopeData(from:to,3);
@@ -28,9 +29,11 @@ load('ref_tracking32.mat') %2 -> Q_y = 100
 input2 = out.ScopeData(from:to,3);
 output2 = out.ScopeData(from:to,2);
 
-load('ref_tracking27.mat') %2 -> Q_y = tuned
-input3 = out.ScopeData(from:to,3);
-output3 = out.ScopeData(from:to,2);
+if tuned == 1
+    load('ref_tracking33.mat') %2 -> Q_y = tuned
+    input3 = out.ScopeData(from:to,3);
+    output3 = out.ScopeData(from:to,2);
+end
 
 set(0,'defaulttextinterpreter','latex')
 lw = 1.5;
@@ -39,20 +42,27 @@ green   = [0.2, 0.7, 0.1];
 
 figure(3), hold on
 plot(time,output1,time,output2,'LineWidth',lw) %only boundaries
-% plot(time,output3,'LineWidth',lw,'Color',green) 
+try
+    plot(time,output3,'LineWidth',lw,'Color',green)   
+end
 plot(time,ref,'--','LineWidth',lw,'Color','k') 
 xlim([0 2600])
 xlabel('time [s]')
 set(gca, 'TickLabelInterpreter','latex','FontSize', fs)
 xlabel('$t$ [s]')
 ylabel('$T$ [$^{\circ}$C]','FontSize', fs)
-legend('$Q_\mathrm{y} = 1000$', '$Q_\mathrm{y} = 100$', 'Interpreter','latex', 'FontSize', fs) %only boundaries
-% legend('$Q_\mathrm{y} = 1000$', '$Q_\mathrm{y} = 100$', '$Q_\mathrm{y}$ tuned', 'Interpreter','latex', 'FontSize', fs) %all
+if tuned == 1
+    legend('$Q_\mathrm{y} = 1000$', '$Q_\mathrm{y} = 100$', '$Q_\mathrm{y}$ tuned', 'Interpreter','latex', 'FontSize', fs) %all
+else
+    legend('$Q_\mathrm{y} = 1000$', '$Q_\mathrm{y} = 100$', 'Interpreter','latex', 'FontSize', fs) %only boundaries
+end
 grid minor
 
 figure(4), hold on
 plot(time,input1,time,input2,'LineWidth',lw) %only boundaries 
-% plot(time,input3,'LineWidth',lw,'Color',green) 
+try
+    plot(time,input3,'LineWidth',lw,'Color',green) 
+end
 plot([0 2600],[20 20],'--','LineWidth',lw,'Color','k')
 plot([0 2600],[100 100],'--','LineWidth',lw,'Color','k')
 axis([0 2600 10 110])
@@ -60,6 +70,9 @@ xlabel('time [s]')
 set(gca, 'TickLabelInterpreter','latex','FontSize', fs)
 xlabel('$t$ [s]')
 ylabel('$U$ [$\%$]','FontSize', fs)
-legend('$Q_\mathrm{y} = 1000$', '$Q_\mathrm{y} = 100$', 'Interpreter','latex', 'FontSize', fs) %only boundaries
-% legend('$Q_\mathrm{y} = 1000$', '$Q_\mathrm{y} = 100$', '$Q_\mathrm{y}$ tuned', 'Interpreter','latex', 'FontSize', fs) %all
+if tuned == 1
+    legend('$Q_\mathrm{y} = 1000$', '$Q_\mathrm{y} = 100$', '$Q_\mathrm{y}$ tuned', 'Interpreter','latex', 'FontSize', fs) %all
+else
+    legend('$Q_\mathrm{y} = 1000$', '$Q_\mathrm{y} = 100$', 'Interpreter','latex', 'FontSize', fs) %only boundaries
+end
 grid minor
