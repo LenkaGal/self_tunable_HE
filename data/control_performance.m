bd05de455198cee3db7431cbff592f1ce4f392f8
Lenka Galcikova <lenka.galcikova@stuba.sk>
2024-04-16 15:55:49 +0200

    review 2nd round

--- data/control_performance.m
+++ data/control_performance.m
@@ -1,5 +1,5 @@
 % clc, close all, clear 
-load('ref_tracking27.mat')
+load('ref_tracking25.mat')
 %% plots
 figure(11)
 hold on; grid minor; box on
--- data/energy_evaluation.m
+++ data/energy_evaluation.m
@@ -1,20 +1,30 @@
-clc, clear all
+clc, clear all, close all
 from = 801;
 to = 6000;
+figure(1), hold on
 
 load('ref_tracking23.mat') %1000
 P_per = out.ScopeData2(from:to+1,3);
 P = P_per/100*2; %kJ/s
 E_1000 = sum(P*Ts) %kJ
+plot(P_per)
 
 load('ref_tracking24.mat') %100
 P_per = out.ScopeData2(from:to+1,3);
 P = P_per/100*2; %kJ/s
 E_100 = sum(P*Ts) %kJ
+plot(P_per)
 
 load('ref_tracking27.mat') %tuned
 P_per = out.ScopeData2(from:to+1,3);
 P = P_per/100*2; %kJ/s
 E = sum(P*Ts) %kJ
+plot(P_per)
+
+% load('ref_tracking25.mat') %500
+% P_per = out.ScopeData2(from:to+1,3);
+% P = P_per/100*2; %kJ/s
+% E_500 = sum(P*Ts) %kJ
+% plot(P_per)
 
 
